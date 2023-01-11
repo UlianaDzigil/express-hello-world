b736e7843282492826c28ee680962fdac9e9e350
UlianaDzigil <68972045+UlianaDzigil@users.noreply.github.com>
2023-01-11 16:09:36 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
